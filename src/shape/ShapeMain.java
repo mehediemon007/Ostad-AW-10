02e5086c488d23bb41a476be353982bd892adc46
Mehedi Hasan Emon <34378422+mehediemon007@users.noreply.github.com>
2024-07-04 23:07:27 +0600

    Update ShapeMain.java

--- src/shape/ShapeMain.java
+++ src/shape/ShapeMain.java
@@ -6,22 +6,22 @@ public class ShapeMain {
 		// TODO Auto-generated method stub
 		
 		
-	Circle circle = new Circle(5.0);
-        Square square = new Square(4.0);
-        Triangle triangle = new Triangle(3.0, 6.0);
-
-        circle.draw();
-        System.out.println("Area of Circle: " + circle.calculateArea());
-
-        System.out.println();
-
-        square.draw();
-        System.out.println("Area of Square: " + square.calculateArea());
-
-        System.out.println();
-
-        triangle.draw();
-        System.out.println("Area of Triangle: " + triangle.calculateArea());
+		Circle circle = new Circle(5.0);
+	        Square square = new Square(4.0);
+	        Triangle triangle = new Triangle(3.0, 6.0);
+	
+	        circle.draw();
+	        System.out.println("Area of Circle: " + circle.calculateArea());
+	
+	        System.out.println();
+	
+	        square.draw();
+	        System.out.println("Area of Square: " + square.calculateArea());
+	
+	        System.out.println();
+	
+	        triangle.draw();
+	        System.out.println("Area of Triangle: " + triangle.calculateArea());
 
 	}
 
